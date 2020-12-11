b30a61dfb9359d58a474cc69ef57e2f0a858cb83
rodrigojpt <rodrigojpereirat@gmail.com>
2020-12-11 14:31:43 -0500

    installed menu and router without styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { slide as Menu } from 'react-burger-menu';
+import { Link, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Series from './Components/Series/Series';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App' id='outer-container'>
+			<Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'} right>
+				<Link to='/'>Home</Link>
+				<Link to='/series'>Browse Series</Link>
+			</Menu>
+			<main id='page-wrap'>
+				<Route path='/' exact component={Home} />
+				<Route path='/series' exact component={Series} />
+			</main>
+		</div>
+	);
 }
 
 export default App;
