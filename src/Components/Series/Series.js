ad389a07bf731bb6c8c398d1965d1e959f3263ff
rodrigojpt <rodrigojpereirat@gmail.com>
2020-12-11 22:27:13 -0500

    added loading spinner component

--- src/Components/Series/Series.js
+++ src/Components/Series/Series.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Series.css';
+import Spinner from '../Spinner/Spinner';
 
 const Series = () => {
-	return <div>Series</div>;
+	const [series, setSeries] = useState(null);
+
+	if (!series) {
+		return (
+			<div className='series-browser'>
+				<h1 className='page-header'>Browse Series</h1>
+				<Spinner />
+			</div>
+		);
+	}
+	return (
+		<div className='series-browser'>
+			<h1 className='page-header'>Browse Series</h1>
+		</div>
+	);
 };
 
 export default Series;
---
+++ src/Components/Spinner/Spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Spinner.css';
+
+/* Spinner code from loading.io converted to component*/
+const Spinner = () => {
+	return (
+		<div class='lds-spinner'>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
+
+export default Spinner;
