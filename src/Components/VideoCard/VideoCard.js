b81d7f1c6f3c7ef00b34f7fbc5773fb744247956
rodrigojpt <rodrigojpereirat@gmail.com>
2020-12-22 15:21:57 -0500

    started redesign of cards, only touched series and needs to be added to for desktop view

--- src/Components/VideoCard/VideoCard.js
+++ src/Components/VideoCard/VideoCard.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './VideoCard.css';
 const VideoCard = ({ video }) => {
+	const secondarySrc = (e) => {
+		console.log('got here');
+		e.target.src = `https://img.youtube.com/vi/${video.ytId}/0.jpg`;
+	};
 	return (
 		<div className='video-card-container'>
 			<Link to={`/videos/${video.id}`} className='video-card'>
-				<img src={`https://img.youtube.com/vi/${video.ytId}/0.jpg`} alt='' />
+				<img
+					src={`https://img.youtube.com/vi/${video.ytId}/maxresdefault.jpg`}
+					onError={secondarySrc}
+					alt=''
+				/>
 				<h3>{video.title}</h3>
 			</Link>
 		</div>
